047c86430a523361e039679320c9558d62862de9
kinoshita <kinoshitaryuta@gmail.com>
2021-04-12 19:39:38 +0900

    Initial commit

---
+++ src/components/Post.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Post.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "PostList.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
